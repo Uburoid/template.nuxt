26452d0638c2e57adb86503908d6b018a50c12d5
alepri51 <39708925+alepri51@users.noreply.github.com>
2018-12-13 18:04:28 +0300

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,10 @@
     <drawer :show="drawer" @drawler="drawer = arguments[0]"/>
     
     <v-toolbar fixed app clipped-left clipped-right flat>
+        <nuxt-link to="/">
+            <img alt="bnMAP.pro" class="top-toolbar-logo" src="~assets/default_user.png" height="32"/>
+        </nuxt-link>
+
         <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
 
         <v-toolbar-title v-text="title"></v-toolbar-title>
