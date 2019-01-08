ef286b873faf3fc24dec007a221869429a60e387
ap51 <alexander.privaloff@gmail.com>
2019-01-09 01:33:29 +0300

    commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,7 @@
 export default async ({ app, store, route, redirect, req, res }) => {
     //debugger
+    store.commit('SET_TITLE', route.path);
+    
     let test = await app.$server.test.get(void 0, { cache: false });
     let analytics = await app.$server.analytics.get({ owner_id: '2874' }, { cache: false });
     let account = await app.$server.account.get(0, { cache: false });
@@ -10,7 +12,7 @@ export default async ({ app, store, route, redirect, req, res }) => {
     //let member = await app.$server.account.signin({ email: 'mychrome51@gmail.com', password: '123' }, { cache: false });
     //debugger
     //await app.$server.account.changePassword({ }, { cache: true });
-    store.commit('SET_TITLE', route.path);
+    
     //store.commit('SET_TITLE', JSON.stringify(account) + '-' + analytics);
 
 }
