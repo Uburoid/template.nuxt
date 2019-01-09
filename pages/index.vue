38ca993e7a801ad672ac2bd1c1448fcbfd4167a9
alepri51 <39708925+alepri51@users.noreply.github.com>
2019-01-09 09:59:53 +0300

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <v-layout column justify-center align-center>
         <v-flex xs12 sm8 md6>
-            <div>HELLO</div>
+            <div>HELLO, WORLD!</div>
         </v-flex>
     </v-layout>
 </template>
