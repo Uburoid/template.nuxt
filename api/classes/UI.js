a56204014ad580cbc67e06a2d6f39ff6b301fec6
ap51 <alexander.privaloff@gmail.com>
2019-01-12 01:41:28 +0300

    commit

--- api/classes/UI.js
+++ api/classes/UI.js
@@ -1,6 +1,5 @@
 
 const { API, SecuredAPI } = require('./base');
-const get_ip = require('ipware')().get_ip;
 
 class UI extends SecuredAPI {
     constructor(...args) {
@@ -9,13 +8,13 @@ class UI extends SecuredAPI {
 
     pageData({ path }) {
         debugger
-        console.log('REQUEST:', this.req.headers['x-forwarded-for'], this.req.client.remoteAddress, this.res.connection.remoteAddress, this.res.socket._sockname);
+        //console.log('REQUEST:', this.req.headers['x-forwarded-for'], this.req.client.remoteAddress, this.res.connection.remoteAddress, this.res.socket._sockname);
 
         path = path === '/' ? 'Welcome to us' : path.slice(1);
 
-        const ip = get_ip(this.req);// this.req.headers['x-forwarded-for'] || (this.req.connection.socket ? this.req.connection.socket.remoteAddress : null) || this.req.socket.remoteAddress || this.req.connection.remoteAddress;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-        path += ` - ${ip.clientIp}`;
+        path += ` - ${ip}`;
         //path += ` - ${this.req.headers['x-forwarded-for'] || this.req.connection.remoteAddress}`
 
         return path;
