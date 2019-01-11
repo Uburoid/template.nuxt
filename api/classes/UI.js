1b59b23cc4e1437b9bf3150e12dd497b5a68372f
ap51 <alexander.privaloff@gmail.com>
2019-01-11 22:15:25 +0300

    commit

--- api/classes/UI.js
+++ api/classes/UI.js
@@ -8,8 +8,8 @@ class UI extends SecuredAPI {
     }
 
     pageData({ path }) {
-
-        console.log('REQUEST:', this.req.headers['x-forwarded-for'], (this.req.connection.socket ? this.req.connection.socket.remoteAddress : null), this.req.socket.remoteAddress, this.req.connection.remoteAddress);
+        debugger
+        console.log('REQUEST:', this.req.headers['x-forwarded-for'], this.req.client.remoteAddress, this.res.connection.remoteAddress, this.res.socket._sockname);
 
         path = path === '/' ? 'Welcome to us' : path.slice(1);
 
