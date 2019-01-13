9bcaf1b9242de709f068a4a1c1b9c05359be5a27
ap51 <alexander.privaloff@gmail.com>
2019-01-13 21:21:22 +0300

    commit

--- ACL/index.js
+++ ACL/index.js
@@ -12,17 +12,21 @@ const ACL = async () => {
     let domain = token_err ? 'expired' : 'not_expired';
     
     const request = {
-        sub: "alice", // the user that wants to access a resource.
-        obj: "data2", // the resource that is going to be accessed.
-        act: 'true' // the operation that the user performs on the resource.
+        action: "show", // the user that wants to access a resource.
+        class: "UI", // the resource that is going to be accessed.
+        method: 'pageData',
+        role: 'Аноним',
+        resource: '/inspire',
+        token: 'invalid'
     }
 
     const e = await ACL();
 
-    if(e.enforce(request.sub, request.obj, request.act) === true) {
-        console.log('ALLOWED');
-    } else {
-        // deny the request, show an error
-        console.log('DENIED');
-    }
+    let perms = e.getPermissionsForUser('Аноним');
+    let perms1 = e.getPermissionsForUser('Пользователь');
+
+    console.log(perms, perms1);
+    
+    e.enforce(request.action, request.class, request.method, request.role, request.resource, 'valid') ? console.log('ALLOWED') : console.log('DENIED');
+    e.enforce(request.action, request.class, request.method, request.role, request.resource, 'invalid') ? console.log('ALLOWED') : console.log('DENIED');
 })();
