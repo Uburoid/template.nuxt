e1480a1d9ad370022779f27c897c8344a71aa87c
ap51 <alexander.privaloff@gmail.com>
2019-01-13 22:35:17 +0300

    commit

--- ACL/index.js
+++ ACL/index.js
@@ -6,8 +6,42 @@ const ACL = async () => {
     return enforcer
 }
 
+const cloudrail = require("cloudrail-si");
+cloudrail.Settings.setKey("5c3a4d4c21b62e5228bbd27a");
+
+
 (async () => {
-    let token_err = {};
+    let selection = 'viber';
+
+    const telegram = new cloudrail.services.Telegram(
+        null,
+        "738767838:AAGisCF0eTHfZ-SJiwUvoyB1mWFYesP0geM",
+        "[Webhook URL]"
+    );
+
+    const viber = new cloudrail.services.Viber(
+        null,
+        "49122a367a27d505-c0559a5494e9a742-246451fd3f9f5a3c",
+        "[Webhook URL]",
+        "BestNovostroy"
+    );
+
+    // 'selection' is a String representing e.g. a user's service choice
+    switch(selection) {
+        case "telegram": service = telegram; break;
+        case "viber": service = viber; break;
+    }
+
+    service.sendMessage(
+        "79009395505",
+        "It's so easy to send message via CloudRail",
+        (error, result) => {
+            // Check for potential error and use the result
+            console.log(error, result)
+        }
+    );
+
+    /* let token_err = {};
 
     let domain = token_err ? 'expired' : 'not_expired';
     
@@ -26,7 +60,10 @@ const ACL = async () => {
     let perms1 = e.getPermissionsForUser('Пользователь');
 
     console.log(perms, perms1);
-    
+
     e.enforce(request.action, request.class, request.method, request.role, request.resource, 'valid') ? console.log('ALLOWED') : console.log('DENIED');
-    e.enforce(request.action, request.class, request.method, request.role, request.resource, 'invalid') ? console.log('ALLOWED') : console.log('DENIED');
+    e.enforce(request.action, request.class, request.method, request.role, request.resource, 'invalid') ? console.log('ALLOWED') : console.log('DENIED'); */
 })();
+
+//49122a367a27d505-c0559a5494e9a742-246451fd3f9f5a3c
+//738767838:AAGisCF0eTHfZ-SJiwUvoyB1mWFYesP0geM
