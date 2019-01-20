f71b8160a481797518bc1f8f87f873d20c83483e
ap51 <alexander.privaloff@gmail.com>
2019-01-20 17:31:35 +0300

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
     <v-layout column justify-center align-center>
         <v-flex xs12 sm8 md6>
             <div>WELCOME TO OUR SITE!</div>
-            <nuxt-link to="/news">HOME PAGE</nuxt-link>
+            <nuxt-link to="/news">NEWS PAGE</nuxt-link>
+            <nuxt-link to="/inspire">INSPIRE PAGE</nuxt-link>
             <nuxt-link to="/signin">SIGN IN</nuxt-link>
             <nuxt-link to="/signup">SIGN UP</nuxt-link>
             <nuxt-link to="/poppo">NOT FOUND</nuxt-link>
