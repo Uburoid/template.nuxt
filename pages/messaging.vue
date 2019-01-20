80bfa1ce9bed2ee96920ca9238f632437e944c00
ap51 <alexander.privaloff@gmail.com>
2019-01-20 22:47:27 +0300

    commit

--- pages/messaging.vue
+++ pages/messaging.vue
@@ -1,8 +1,9 @@
 <template>
-        <v-card style="height: 100%; display: flex; flex-direction: column; justify-content: space-between">
+    <v-layout align-center justify-center column fill-height>
+        <v-card>
             <v-card-title>
                 <v-icon class="mr-1 primary--text">fas fa-sign-in-alt</v-icon>
-                <span class="headline primary--text">Вход</span>
+                <span class="headline primary--text">Отправить сообщение</span>
             </v-card-title>
             <v-card-text>
                 <v-card-text>
@@ -48,6 +49,7 @@
             {{ result }}
 
         </v-card>
+    </v-layout>
 </template>
 
 <script>
