819ff2e1b74d427bb9dff13df47d5219e4decbc3
ap51 <alexander.privaloff@gmail.com>
2019-01-29 00:32:20 +0300

    commit

--- cicd.js
+++ cicd.js
@@ -1,42 +1,35 @@
 const shell = require('shelljs');
 
-try {
-    let cd = shell.cd(process.cwd());
-    console.log(`cd: ${cd}`);
+let cd = shell.cd(process.cwd());
+console.log(`cd: ${cd}`);
 
-    //let stop = shell.exec('pm2 stop all');
-    //console.log(`stop: ${stop}`);
+//let stop = shell.exec('pm2 stop all');
+//console.log(`stop: ${stop}`);
 
-    let stash = shell.exec('git stash');
-    console.log(`pull: ${stash}`);
+let stash = shell.exec('git stash');
+console.log(`pull: ${stash}`);
 
-    let pull = shell.exec('git pull');
-    console.log(`pull: ${pull}`);
+let pull = shell.exec('git pull');
+console.log(`pull: ${pull}`);
 
-    let npm = true;
-    /* let npm = req.body.commits.some(commit => {
-        return commit.modified.includes('package.json');
-    }); */
+let npm = true;
+/* let npm = req.body.commits.some(commit => {
+    return commit.modified.includes('package.json');
+}); */
 
-    if(npm) {
-        console.log(`npm operations strarting...`);
+if(npm) {
+    console.log(`npm operations strarting...`);
 
-        let install = shell.exec('npm install');
-        console.log(`install: ${install}`);
+    let install = shell.exec('npm install');
+    console.log(`install: ${install}`);
 
-        let update = shell.exec('npm update');
-        console.log(`update: ${update}`);
-    }
-
-
-    let update = shell.exec('npm run build');
+    let update = shell.exec('npm update');
     console.log(`update: ${update}`);
+}
 
-    let restart = shell.exec('pm2 restart all');
-    console.log(`restart: ${restart}`);
-    
-    //resolve('ok');
-}
-catch(err) {
-    console.log(`ERROR: ${err}`);
-}
+
+let update = shell.exec('npm run build');
+console.log(`update: ${update}`);
+
+let restart = shell.exec('pm2 restart all');
+console.log(`restart: ${restart}`);
