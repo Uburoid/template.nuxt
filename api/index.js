721509631390a053e22959f5df5b19cb65106060
ap51 <alexander.privaloff@gmail.com>
2019-01-29 01:57:19 +0300

    commit

--- api/index.js
+++ api/index.js
@@ -526,7 +526,7 @@ router.all('/rebuild', async (req, res, next) => {
     const { spawn } = require('child_process');
 
     const npm = spawn('npm', ['install']);
-    console.log(`npm install ${npm}`);
+    console.log(`npm initial install`);
     
     npm.on('close', (code) => {
         console.log(`npm done ${code}`);
