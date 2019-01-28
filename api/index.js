abf1b3c7d5f6ee0080cb3630e3d67dc7a01bd672
ap51 <alexander.privaloff@gmail.com>
2019-01-29 01:19:20 +0300

    commit

--- api/index.js
+++ api/index.js
@@ -533,7 +533,7 @@ router.all('/rebuild', async (req, res, next) => {
     ls.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
 
-        console.log(`BUILD DONE?: ${restart}`);
+        console.log(`BUILD DONE? ${data}`);
         setTimeout(() => {
             let restart = shell.exec('pm2 restart all');
             console.log(`restart: ${restart}`);
