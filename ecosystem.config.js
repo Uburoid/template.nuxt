a2da3b2f5d0e951287e5d638e2375318b3f092c2
ap51 <alexander.privaloff@gmail.com>
2019-01-29 21:47:33 +0300

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,17 @@ module.exports = {
       watch: true,
       env: {
         NODE_ENV: "development",
+        API_HOST: "node.ap51.tech",
+        //API_PORT=
+        API_PROTOCOL: "https",
+        API_SUFFIX: "/api"
       },
       env_production: {
         NODE_ENV: "production",
+        API_HOST: "node.ap51.tech",
+        //API_PORT=
+        API_PROTOCOL: "https",
+        API_SUFFIX: "/api"
       }
     }]
   }
