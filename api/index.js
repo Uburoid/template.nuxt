0f1f2140e907228a7985c165ee4dd1b59915211f
ap51 <alexander.privaloff@gmail.com>
2019-01-29 23:00:36 +0300

    commit

--- api/index.js
+++ api/index.js
@@ -539,7 +539,7 @@ router.all('/rebuild', async (req, res, next) => {
         console.log(`stderr: ${stderr}`);
     }); */
 
-    let child = spawn('./deploy.sh');
+    let child = spawn('bash', ['deploy.sh']);
 
     child.stdout.on('data', (data) => {
         console.log(`child stdout: ${data}`);
