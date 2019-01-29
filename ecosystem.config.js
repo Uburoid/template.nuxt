3ad8cd022efd4db8fa8d2f6672a4959c4d3e9cf7
ap51 <alexander.privaloff@gmail.com>
2019-01-29 21:39:02 +0300

    commit

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+      name: "app",
+      script: "npm run build && npm run start",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }]
+  }
