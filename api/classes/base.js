71e3185325111597a895c2f84f5726a3e9fe2a4a
ap51 <alexander.privaloff@gmail.com>
2019-02-04 01:59:30 +0300

    COMMIT

--- api/classes/base.js
+++ api/classes/base.js
@@ -308,7 +308,7 @@ class API extends Base {
         }
         else this.payload = await this.jwt.verify(this.token);
 
-        if(this.res.locals && this.res.locals.token_expired) {
+        if(this.res.locals && this.res.locals.token_expired) { //redundant code
             const { Account } = require('./account');
             this.payload = await Account.signout(this.payload);
         }
